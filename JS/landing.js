e8430952f3d9dc50ba0f9f349fa06630618c61aa
Annie <msannieelizabeth@gmail.com>
2017-10-06 10:33:34 -0600

    Update index page, add index Javascript, some other changes. Changes to about page.

--- JS/landing.js
+++ JS/landing.js
@@ -1,3 +1,12 @@
 $(window).scroll(function(){
     $(".hero-title, .hero-subtitle, .hero-text").css("opacity", 1 - $(window).scrollTop() / 250);
   });
+
+
+$(document).on('click', 'a[href^="#view"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
