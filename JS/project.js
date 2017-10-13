94785146f00d2eb827c146ae44a9ebc22212e3bd
Annie <msannieelizabeth@gmail.com>
2017-10-12 23:42:41 -0600

    Almost done with The Hungry Pear.

---
+++ JS/project.js
@@ -0,0 +1,12 @@
+$(window).scroll(function(){
+    $(".my-name, .name-detail, .logo").css("opacity", 1 - $(window).scrollTop() / 250);
+  });
+
+
+$(document).on('click', 'a[href^="#spot"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
